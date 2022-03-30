11988a17985cf9405fe12599da36e9beb504a4a0
Devin Buhl <onedr0p@users.noreply.github.com>
2022-03-30 07:19:51 -0400

    Remove projector apps (#193)
    * Delete apps/projector-clion directory
    
    * Delete apps/projector-datagrip directory
    
    * Delete apps/projector-goland directory
    
    * Delete apps/projector-idea-c directory
    
    * Delete apps/projector-idea-u directory
    
    * Delete apps/projector-phpstorm directory
    
    * Delete apps/projector-pycharm-c directory
    
    * Delete apps/projector-pycharm-p directory
    
    * Delete apps/projector-webstorm directory

--- apps/projector-goland/latest-version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-version=$(curl -sX GET "https://data.services.jetbrains.com/products?code=GO&release.type=release" | jq -r '.[0].releases[0].version')
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
