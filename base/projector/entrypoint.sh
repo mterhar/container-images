1a7b69bc297ca8d9460316490280990d29d525e4
Devin Buhl <devin@buhl.casa>
2022-06-06 21:53:59 -0400

    fix: housekeeping
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- base/projector/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#shellcheck disable=SC1091
-source "/shim/umask.sh"
-source "/shim/vpn.sh"
-source "/shim/jks.sh"
-source "/shim/ide.sh"
-
-exec "$IDE_RUN_FILE_NAME-projector.sh"
