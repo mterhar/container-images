1678dd4c94927f10fb63b9c87d4421a28d0d48c2
Devin Buhl <onedr0p@users.noreply.github.com>
2022-06-20 00:02:31 -0400

    fix: emby entrypoint script

--- apps/emby/entrypoint.sh
+++ apps/emby/entrypoint.sh
@@ -4,16 +4,17 @@
 source "/shim/umask.sh"
 source "/shim/vpn.sh"
 
+APP_DIR="/app/emby"
 export LD_LIBRARY_PATH="${APP_DIR}"
-export FONTCONFIG_PATH="${APP_DIR}"/etc/fonts
+export FONTCONFIG_PATH="${APP_DIR}/etc/fonts"
 if [ -d "/lib/x86_64-linux-gnu" ]; then
-	export LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri:"${APP_DIR}"/dri
+	export LIBVA_DRIVERS_PATH="/usr/lib/x86_64-linux-gnu/dri:${APP_DIR}/dri"
 fi
-export SSL_CERT_FILE="${APP_DIR}"/etc/ssl/certs/ca-certificates.crt
+export SSL_CERT_FILE="${APP_DIR}/etc/ssl/certs/ca-certificates.crt"
 
-exec /app/EmbyServer \
+exec /app/emby/EmbyServer \
 	-programdata /config \
-	-ffdetect /app/ffdetect \
-	-ffmpeg /app/ffmpeg \
-	-ffprobe /app/ffprobe \
+	-ffdetect /app/emby/ffdetect \
+	-ffmpeg /app/emby/ffmpeg \
+	-ffprobe /app/emby/ffprobe \
 	-restartexitcode 3
