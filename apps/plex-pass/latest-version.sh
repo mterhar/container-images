12d9ae199c897ec05978d28cece52ea63e475c05
Devin Buhl <devin.kray@gmail.com>
2022-06-21 19:05:44 -0400

    feat: add plex-pass image
    Signed-off-by: Devin Buhl <devin.kray@gmail.com>

---
+++ apps/plex-pass/latest-version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rm -rf plex-media-server-plexpass
+git clone --quiet --depth=1 https://aur.archlinux.org/plex-media-server-plexpass.git
+
+version="$(grep -oP "(?<=pkgver=).*" ./plex-media-server-plexpass/PKGBUILD)"
+version="${version}-$(grep -oP "(?<=_pkgsum=).*" ./plex-media-server-plexpass/PKGBUILD)"
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
